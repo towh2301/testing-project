2e680af42b4936c1cac80ea5f481632f3b1fe7f9
Bui Hoang Huy <74142615+towh2301@users.noreply.github.com>
2024-01-26 19:09:12 +0700

    update

---
+++ src/main/java/com/myteam/testing_project/Testing_project.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.myteam.testing_project;
+
+/**
+ *
+ * @author huybu
+ */
+public class Testing_project {
+
+    public static void main(String[] args) {
+        new IndexFrame().setVisible(true);
+    }
+}
